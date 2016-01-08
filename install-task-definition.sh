1661d2a754ab09c385dc3bb9599976e0ce1e103e
Ron Kurr <kurr@kurron.org>
2016-01-08 19:25:12 +0000

    Can install the task definition

--- install-task-definition.sh
+++ install-task-definition.sh
@@ -9,13 +9,13 @@
 # endif
 # transform artifact group into path com.transparent.al becomes com/transparent/al
 # need artifact id, eg. hid-generator
-# transform branch, major, minor, patch into a release, eg. 1.0.10.RELEASE
+# transform branch, major, minor, patch into a version, eg. 1.0.10.RELEASE
 # combine those pieces into a final URL
 
-BRANCH=master
+BRANCH=development
 MAJOR=1
 MINOR=0
-PATCH=10
+PATCH=6
 GROUP=com.transparent.al
 GROUP_PATH=$(echo ${GROUP} | tr . /)
 RELEASE_URL=http://192.168.254.81:81/artifactory/transparent-release
@@ -41,3 +41,8 @@ eval ${SERVICE_CMD}
 UPDATE_CMD="curl --get --silent --show-error --output /tmp/service-update.json ${URL}/${GROUP_PATH}/${ARTIFACT_ID}/${VERSION}/${ARTIFACT_ID}-${VERSION}-service-update.json"
 echo ${UPDATE_CMD}
 eval ${UPDATE_CMD}
+
+INSTALL_TASK_CMD="/usr/local/bin/aws ecs register-task-definition --region ${AWS_REGION} --cli-input-json file:///tmp/task-definition.json"
+echo ${INSTALL_TASK_CMD}
+eval ${INSTALL_TASK_CMD}
+
